f1dd886a937091a3b7cbb4648eaa1cdd04c588af
JuanCarlosHenaoLopez <102966888+JuanCarlosHenaoLopez@users.noreply.github.com>
2023-04-24 11:27:37 -0500

    Se reciben los inmuebles

--- src/App.js
+++ src/App.js
@@ -5,17 +5,140 @@ import './styles/index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import dataScene from './helpers/dataScene';
 import EscenaSola from './components/escenasola'
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
   const [sceneSelected, setSceneSelected] = useState(false);
   const [numberScene, setNumberScene] = useState(0);
+  const [properties, setProperties] = useState([]);
+
   const handleClick = (id) => {
+
     console.log('oe---');
     console.log(id);
     setSceneSelected(true);
     setNumberScene(id);
   };
+
+  // const getProperties = async ()=>{
+  //   const url = 'http://127.0.0.1:8080/inmobiliaria360/inmuebles'
+  //   // const url = 'https://gorest.co.in/public/v2/users'
+  //   // const config = {
+  //   //   headers: {
+  //   //     'Accept-Encoding': 'gzip, deflate, br',
+  //   //     'Host':'<calculated when request is sent>'
+  //   //   }
+  //   // };
+  //   let properties = []
+  //   try {
+  //     properties = await axios.get(url)
+  //   } catch (error) {
+  //     console.log(error)
+  //   }
+  //   console.log("Prueba 1")
+  //   console.log(properties.data)
+  
+  //   return { properties};
+  // }
+
+
+
+  // useEffect(async () => {
+  //   setProperties(await getProperties());
+  //   postInmueble();
+  // }, [properties]);
+
+
+
+
+    // MIO 
+  // const getProperties = () =>{
+  //   let properties = []
+  //   axios({
+  //     method:"GET",
+  //     url:"http://127.0.0.1:8080/inmobiliaria360/inmuebles"
+  //   }).then(    properties = axios());
+  // }
+
+
+
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     // You can await here
+  //     const response = await getProperties();
+  //     setProperties(response)
+  //     // ...
+  //   }
+  //   fetchData();
+  // }, [properties]); // Or [] if effect doesn't need props or state
+
+
+  // const url = "http://localhost:8080/inmobiliaria360/inmuebles"
+  // React.useEffect(()=>{
+  //   axios.get(url).then((response) => {
+  //     setProperties(response.data);
+  //   });
+  // },[])
+
+
+
+  // const getProperties = async ()=>{
+  //   const url = 'http://127.0.0.1:8080/inmobiliaria360/inmuebles'
+  //   let properties = []
+  //   try {
+  //     properties = await axios.get(url)
+  //   } catch (error) {
+  //     console.log(error)
+  //   }
+  //   console.log("Prueba 1")
+  //   console.log(properties.data)
+  
+  //   return { properties};
+  // }
+
+
+
+  // useEffect(async () => {
+  //   setProperties(await getProperties())
+  // }, [properties]);
+
+  const postInmueble = () =>{
+    axios({
+      method: 'post',
+      url: 'http://localhost:8080/inmobiliaria360/publicarInmueble',
+      data: {
+        name: "Finca La Ceja  - nueva ",
+        price: 50.00,
+        description: "Ubicado en La Ceja",
+        image: "https://res.cloudinary.com/back-pragma/image/upload/v1681707044/tesis/Prueba1_m4xukw.jpg"
+      }
+    });
+  }
+
+
+/// -------------------------------------------------------------------------
+  /// Ejempplo de consumo 
+
+  const url = "http://127.0.0.1:8080/inmobiliaria360/inmuebles";
+
+
+  const getInmuebles = () => {
+    fetch(url)
+      .then(response => response.json())
+      // .then(data => console.log(data))
+      .then(data => setProperties(data))
+      .catch(error => console.log(error))
+  };
+
+
+  useEffect( () => {
+    getInmuebles(url);
+    postInmueble();
+  }, []);
+
+/// -------------------------------------------------------------------------
+
 
   return (
     <div className="App">
@@ -27,11 +150,15 @@ function App() {
           </div>
         )}
         { !sceneSelected  && (
-          <Properties handleClick={handleClick}></Properties>
+          <Properties properties={properties} handleClick={handleClick}></Properties>
         )}
       </div>
     </div>
   );
 }
 
+
+
 export default App;
+
+
--- src/components/propertyList.js
+++ src/components/propertyList.js
@@ -1,34 +1,34 @@
 import Property from "./property";
 
-const Properties = (props) => {
-    const properties = [{
-        id: 1,
-        name: "La Ceja",
-        price: 599,
-        description: "Inbmobiliria 1",
-        image: "https://codingyaar.com/wp-content/uploads/card-image-boat-scaled.jpg"
-    },
-    {
-        id: 2,
-        name: "El carmen de viboral",
-        price: 810,
-        description: "Inbmobiliria 2",
-        image: "https://codingyaar.com/wp-content/uploads/card-image-boat-scaled.jpg"
-    },
-    {
-        id: 3,
-        name: "Rionegro",
-        price: 4000,
-        description: "Inbmobiliria 4",
-        image: "https://codingyaar.com/wp-content/uploads/card-image-boat-scaled.jpg"
-    },
-    {
-        id: 4,
-        name: "Rionegro",
-        price: 400,
-        description: "Inbmobiliria 5",
-        image: "https://codingyaar.com/wp-content/uploads/card-image-boat-scaled.jpg"
-    }]
+const Properties = (props) => {  // props sería el arreglo de inmuebles 
+    // const properties = [{
+    //     id: 1,
+    //     name: "La Ceja",
+    //     price: 599,
+    //     description: "Inbmobiliria 1",
+    //     image: "https://codingyaar.com/wp-content/uploads/card-image-boat-scaled.jpg"
+    // },
+    // {
+    //     id: 2,
+    //     name: "El carmen de viboral",
+    //     price: 810,
+    //     description: "Inbmobiliria 2",
+    //     image: "https://codingyaar.com/wp-content/uploads/card-image-boat-scaled.jpg"
+    // },
+    // {
+    //     id: 3,
+    //     name: "Rionegro",
+    //     price: 4000,
+    //     description: "Inbmobiliria 4",
+    //     image: "https://res.cloudinary.com/back-pragma/image/upload/v1681708744/Prueba1_gq9uij.jpg"
+    // },
+    // {
+    //     id: 4,
+    //     name: "Rionegro",
+    //     price: 400,
+    //     description: "Inbmobiliria 5",
+    //     image: "https://res.cloudinary.com/back-pragma/image/upload/v1681708379/cnbtjijopxu8my7bkfhf.png"
+    // }]
 
     return (
         <div class='col-9'>
@@ -36,9 +36,9 @@ const Properties = (props) => {
                 <h1>Inmuebles</h1>
             </div>
             <div className='row'>
-            {properties?.map((property) => {
+            {props.properties.map((property) => {
                 return (
-                    <div className='col-4'>
+                    <div className='col-4'>  {/* añadir key  */}
                     <Property
                         id={property.id}
                         description={property.description}
